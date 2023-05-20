245f78c34fe0ef1ee5d9a172e20b4a6d8ffe2372
Miguel Angel <angelbetancur.zapata@gmail.com>
2023-05-20 16:03:13 -0500

    Added new user, view user, remove user, function

--- userRegistrationSystem.java
+++ userRegistrationSystem.java
@@ -31,7 +31,6 @@ public class userRegistrationSystem {
             }
         }
     }
-
     private static void showMenu() {
         System.out.println("Menu:");
         System.out.println("1. Register user");
@@ -40,5 +39,72 @@ public class userRegistrationSystem {
         System.out.println("4. Exit");
         System.out.print("Enter option > ");
     }
+    private static int getUserInput() {
+        return scanner.nextInt();
+    }
+    private static void registerUser() {
+        System.out.print("Enter the number of users to register: ");
+        int count = scanner.nextInt();
+        scanner.nextLine(); // Consume newline character
 
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Register User #" + i);
+            System.out.print("Enter name > ");
+            String name = scanner.nextLine();
+            System.out.print("Enter ID > ");
+            String id = scanner.nextLine();
+            System.out.print("Enter position > ");
+            String position = scanner.nextLine();
 
+            User user = new User(name, id, position);
+            userList.add(user);
+            System.out.println("User registered successfully.\n");
+        }
+    }
+    private static void viewUsers() {
+        System.out.println("List of users:");
+        for (User user : userList) {
+            System.out.println(user.getName());
+            System.out.println(user.getId());
+            System.out.println(user.getPosition());
+            System.out.println();
+        }
+    }
+    private static void removeUser() {
+        System.out.print("Enter the user ID to remove: ");
+        String id = scanner.next();
+        boolean removed = false;
+
+        for (User user : userList) {
+            if (user.getId().equals(id)) {
+                userList.remove(user);
+                removed = true;
+                break;
+            }
+        }
+        if (removed) {
+            System.out.println("User with ID " + id + " removed successfully.\n");
+        } else {
+            System.out.println("User with ID " + id + " not found.\n");
+        }
+    }
+    private static class User {
+        private String name;
+        private String id;
+        private String position;
+        public User(String name, String id, String position) {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+        }
+        public String getName() {
+            return name;
+        }
+        public String getId() {
+            return id;
+        }
+        public String getPosition() {
+            return position;
+        }
+    }
+}
